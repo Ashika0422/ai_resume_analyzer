44de04ab4eaf20f92bf5ed1101b27ee9c4584465
Ashika0422 <dandeniyaashika@gmail.com>
2026-02-25 09:53:45 +0530

    Navbar Created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')]">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Get instant feedback on your resume and job applications</h2>
+      </div>
+    </section>
+
+
+  </main>
 }
